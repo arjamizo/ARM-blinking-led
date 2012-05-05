559d7dff7ee6edd25de44e7189c2e0e9e09c1743
Artur Zochniak <arjamizo@gmail.com>
2012-05-05 23:38:00 +0200

    initial commit

---
+++ ledBlinking.cob
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Project>
+  <Device>
+    <Manufacturer id="ST" name="ST"/>
+    <Chip id="STM32F103VC" name="STM32F103VC"/>
+  </Device>
+  <Components>
+    <Component id="bf7c3c91-96ed-11df-80ae-001d7d723e56" name="CMSIS core"/>
+    <Component id="bfa102a1-96ed-11df-80ae-001d7d723e56" name="RCC"/>
+    <Component id="bfbf8721-96ed-11df-80ae-001d7d723e56" name="PWR"/>
+    <Component id="bfe42621-96ed-11df-80ae-001d7d723e56" name="GPIO"/>
+    <Component id="0e2e20b0-4bb6-11e0-8e4a-001d7dcd0baf" name="CMSIS Boot"/>
+  </Components>
+  <ExcludedFiles/>
+  <Drivers/>
+</Project>
---
+++ readme.txt
@@ -0,0 +1,14 @@
+done for stm32f103vc
+
+Howto: 
+
+rquirements 
+• CoIDE V1.4.1, http://www.coocox.org/Downloads.htm
+• GCC ARM Embedded, http://coocox.org/CoIDE/Compiler_Settings.html
+
+optional dcumentation: 
+• HY-MiniSTM32V_SCH.pdf (for led connections) 
+
+select manufacturer:st>stm32f103vc
+modules: pwr, gpio
+
